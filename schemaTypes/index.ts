06cb4c88baece2e9411050744358be70d84416a3
Mafaz0206 <mafaz@mavencart.com>
2026-09-25 10:19:59 +0530

    feat: create the employee pages

--- schemaTypes/index.ts
+++ schemaTypes/index.ts
@@ -1,6 +1,7 @@
 import contact from "./contact";
+import employee from "./employee";
 import message from "./message";
 import portfolioCategory from "./portfolioCategory";
 import work from "./work";
 
-export const schemaTypes = [contact, message, work, portfolioCategory];
+export const schemaTypes = [contact, message, work, portfolioCategory, employee];
